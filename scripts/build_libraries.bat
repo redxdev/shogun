bb824f42f27d03df61f8cb3de4193241d1cbb78f
Sam Bloomberg <sam@redxdev.com>
2014-10-10 15:26:23 -0400

    updated batch scripts

--- scripts/build_libraries.bat
+++ scripts/build_libraries.bat
@@ -1,11 +1,4 @@
 @echo off
 
-echo --- BUILDING standard.sxl ---
-sholan lib\standard.build.sl lib\standard.build.shasm
-shasm -i lib\standard.build.shasm -o lib\standard.sxl
-echo --- FINISHED standard.sxl ---
-
-echo --- BUILDING test.sxl ---
-sholan lib\test.build.sl lib\test.build.shasm
-shasm -i lib\test.build.shasm -o lib\test.sxl
-echo --- FINISHED test.sxl ---
+call build_single_library.bat standard
+call build_single_library.bat test
